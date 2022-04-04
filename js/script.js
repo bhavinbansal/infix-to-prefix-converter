3cb7504c4f454de42125dfcee1be7148ae88c3cd
Bhavin Bansal <bhavinbansal7865@gmail.com>
2022-04-05 01:13:52 +0530

    Added script.js for infix-to-prefix converter

---
+++ js/script.js
@@ -0,0 +1,108 @@
+// Created an empty array
+var stackarr = [];
+
+// Variable topp initialized with -1
+var topp = -1;
+
+// Push function for pushing
+// elements inside stack
+function push(e) {
+	topp++;
+	stackarr[topp] = e;
+}
+
+// Pop function for returning top element
+function pop() {
+	if (topp == -1)
+		return 0;
+	else {
+		var popped_ele = stackarr[topp];
+		topp--;
+		return popped_ele;
+	}
+}
+
+// Function to check whether the passed
+// character is operator or not
+function operator(op) {
+	if (op == '+' || op == '-' || op == '^' || op == '*' || op == '/' || op == '(' || op == ')')
+	{
+		return true;
+	}
+	else
+		return false;
+}
+
+// Function to return the precedency of operator
+function precedency(pre) {
+	if (pre == '@' || pre == '(' || pre == ')')
+	{
+		return 1;
+	}
+	else if (pre == '+' || pre == '-')
+	{
+		return 2;
+	}
+	else if (pre == '/' || pre == '*')
+	{
+		return 3;
+	}
+	else if (pre == '^')
+	{
+		return 4;
+	}
+	else
+		return 0;
+}
+
+
+function InfixtoPrefix()
+{
+    var prefix=[];
+    var temp=0;
+    push('@');
+    infixval= document.getElementById("infixvalue").value;
+
+    for(var i=infixval.length-1;i>=0;i--)
+    {
+        var el=infixval[i];
+        if(operator(el))
+        {
+            if (el =='(') {
+                while (stackarr[topp] != ")") {
+                  prefix[temp++] = pop();
+                }
+          pop();
+            }
+            else if(el==')')
+            {
+                push(el);
+            }
+
+            else if(precedency(el)>precedency(stackarr[topp]))
+            {
+                push(el);
+            }
+            else
+            {
+                while(precedency(el)<=precedency(stackarr[topp])&&topp>-1)
+                {
+                     prefix[temp++]=pop();
+                }
+                push(el);
+            }
+        }
+        else{
+            prefix[temp++]=el;
+        }
+
+        
+    }
+    while(stackarr[topp]!='@')
+    {
+        prefix[temp++]=pop();
+    }
+    var st="";
+    for(var i=prefix.length-1;i>=0;i--)st+=prefix[i];
+   document.getElementById("pretext").innerHTML=st;
+ }
